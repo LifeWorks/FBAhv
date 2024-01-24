1ce2034765813e473e26ba97ac2c33f2b4a96024
Song Feng <song.feng@outlook.com>
2024-01-24 14:38:31 -0800

    update

--- modelAnalysis/info.py
+++ modelAnalysis/info.py
@@ -3,35 +3,35 @@
 
 # Metabolite (verbose to model) definitions
 metDict = {
-    'atp': 'atp[c]',        # ATP,              ChEBI 15422
-    'ctp': 'ctp[c]',        # CTP,              ChEBI 17677
-    'gtp': 'gtp[c]',        # GTP,              ChEBI 15996
-    'utp': 'utp[c]',        # UTP,              ChEBI 15713
-    'A': 'ala-L[c]',        # Alaline,          ChEBI 16977
-    'R': 'arg-L[c]',        # Arginine,         ChEBI 16467
-    'N': 'asn-L[c]',        # Asparagine,       ChEBI 17196
-    'D': 'asp-L[c]',        # Aspartate,        ChEBI 17053
-    'C': 'cys-L[c]',        # Cysteine,         ChEBI 17561
-    'Q': 'gln-L[c]',        # Glutamine,        ChEBI 18050
-    'E': 'glu-L[c]',        # Glutamate,        ChEBI 16015
-    'G': 'gly[c]',          # Glycine,          ChEBI 15428
-    'H': 'his-L[c]',        # Histidine,        ChEBI 15971
-    'I': 'ile-L[c]',        # Isoleucine,       ChEBI 17191
-    'L': 'leu-L[c]',        # Leucine,          ChEBI 15603
-    'K': 'lys-L[c]',        # Lysine,           ChEBI 18019
-    'M': 'met-L[c]',        # Methionine,       ChEBI 16643
-    'F': 'phe-L[c]',        # Phenylalanine,    ChEBI 17295
-    'P': 'pro-L[c]',        # Proline,          ChEBI 17203
-    'S': 'ser-L[c]',        # Serine,           ChEBI 17115
-    'T': 'thr-L[c]',        # Threonine,        ChEBI 16857
-    'W': 'trp-L[c]',        # Tryptophan,       ChEBI 16828
-    'Y': 'tyr-L[c]',        # Tyrosine,         ChEBI 17895
-    'V': 'val-L[c]',        # Valine,           ChEBI 16414
-    'h2o': 'h2o[c]',        # H2O
-    'adp': 'adp[c]',        # ADP
-    'Pi': 'pi[c]',          # Phosphate
-    'h': 'h[c]',            # Hydrogen [Proton]
-    'PPi': 'ppi[c]',        # Pyrophosphate
+    'atp': 'ATP[c]',        # ATP,              ChEBI 15422
+    'ctp': 'CTP[c]',        # CTP,              ChEBI 17677
+    'gtp': 'GTP[c]',        # GTP,              ChEBI 15996
+    'utp': 'UTP[c]',        # UTP,              ChEBI 15713
+    'A': 'L_Alanine[c]',        # Alanine,          ChEBI 16977
+    'R': 'L_Arginine[c]',        # Arginine,         ChEBI 16467
+    'N': 'L_Asparagine[c]',        # Asparagine,       ChEBI 17196
+    'D': 'L_Aspartate[c]',        # Aspartate,        ChEBI 17053
+    'C': 'L_Cysteine[c]',        # Cysteine,         ChEBI 17561
+    'Q': 'L_Glutamine[c]',        # Glutamine,        ChEBI 18050
+    'E': 'L_Glutamate[c]',        # Glutamate,        ChEBI 16015
+    'G': 'Glycine[c]',          # Glycine,          ChEBI 15428
+    'H': 'L_Histidine[c]',        # Histidine,        ChEBI 15971
+    'I': 'L_Isoleucine[c]',        # Isoleucine,       ChEBI 17191
+    'L': 'L_Leucine[c]',        # Leucine,          ChEBI 15603
+    'K': 'L_Lysine[c]',        # Lysine,           ChEBI 18019
+    'M': 'L_Methionine[c]',        # Methionine,       ChEBI 16643
+    'F': 'L_Phenylalanine[c]',        # Phenylalanine,    ChEBI 17295
+    'P': 'L_Proline[c]',        # Proline,          ChEBI 17203
+    'S': 'L_Serine[c]',        # Serine,           ChEBI 17115
+    'T': 'L_Threonine[c]',        # Threonine,        ChEBI 16857
+    'W': 'L_Tryptophan[c]',        # Tryptophan,       ChEBI 16828
+    'Y': 'L_Tyrosine[c]',        # Tyrosine,         ChEBI 17895
+    'V': 'L_Valine[c]',        # Valine,           ChEBI 16414
+    'h2o': 'H2O[c]',        # H2O
+    'adp': 'ADP[c]',        # ADP
+    'Pi': 'Orthophosphate[c]',          # Phosphate
+    'h': 'H[c]',            # Hydrogen [Proton]
+    'PPi': 'Diphosphate[c]',        # Pyrophosphate
 }
 
 # Nucleotides Dictionary
@@ -71,8 +71,8 @@ miscDict = {
 # Avogadro's Number
 N_A         = 6.0221409e+23
 # ATP requirement coefficients
-# source: Queka, Dietmaira, Hanschob, Martíneza, Borthb, Nielsen
-# Journal of Biotechnology 184 (2014) 172–178
+# source: Queka, Dietmaira, Hanschob, Martï¿½neza, Borthb, Nielsen
+# Journal of Biotechnology 184 (2014) 172ï¿½178
 k_atp_protein = 4.3
 k_atp_dna = 1.4
 k_atp_rna = 0.4
